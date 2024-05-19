ea0588cff717737b436b976522245354fb22b69d
muhammadsharjeel2k22skipq <muhammad.sharjeel.sirius@gmail.com>
2024-05-19 13:51:54 +0500

    Basic structure for hono

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,26 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
+app.use('*', clerkMiddleware());
 
-app.get('/hello', (c) => {
-    return c.json({
-        message: 'Hello nextjs!',
-    });
-})
+app
+    .get('/hello', (c) => {
+        const auth = getAuth(c);
+        if(!auth?.userId) return c.json({
+            error: 'Unauthorized',
+        })
+        return c.json({
+            message: 'Hello nextjs!',
+            userId: auth?.userId,
+        });
+    })
+
 
 export const GET = handle(app);
 export const POST = handle(app);
